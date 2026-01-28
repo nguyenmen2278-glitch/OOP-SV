25d3e383882b7ba79d2dc1bbba7d64929e64247a
nguyenmen2278-glitch <nguyenmen2278@gmail.com>
2026-01-28 13:40:34 +0700

    Upload Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using OOP_1;
+using System;
+using System.Text;
+
+namespace OOP_B1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+           
+            sinhvien[] dsSV = new sinhvien[2];
+
+           
+            Console.WriteLine("--- NHẬP THÔNG TIN 2 SINH VIÊN ---");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"Nhập sinh viên thứ {i + 1}:");
+                dsSV[i] = new sinhvien(); 
+                dsSV[i].NhapSV();
+            }
+
+           
+            Console.WriteLine("\n--- Hien thi thong tin sinh vien ---");
+            for (int i = 0; i < 2; i++)
+            {
+                dsSV[i].DisplaySV(); 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
